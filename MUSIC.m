296ee39bcdf97c56bc9c728b53048c2361204896
Mousias19 <63747941+Mousias19@users.noreply.github.com>
2022-05-17 16:20:49 +0300

    Update MUSIC.m

--- MUSIC.m
+++ MUSIC.m
@@ -1,36 +1,47 @@
 function [degrees,delay] = MUSIC(Rsig,M,N,d,lam,subcarr)
-    Rsig = reshape(Rsig,[],1);
+    Rsig = Rsig.';
+    Rsig = Rsig(:);
 %   MUSIC
     %   Auto-correlation matrix
-     Rxx = Rsig*Rsig'/subcarr/2;
+     Rxx = Rsig*Rsig';
      
-     % eigen decomposition technique #1   
-     [eigVector, eigValue] = eig(Rxx);
-     eigValue = (abs(diag(eigValue)));
-     % signal and noise eigenvalue threshold
-     eigNoiseThr = 0.01;
-     % signal subspace
-     eigVectSignal = eigVector(:, eigValue > eigNoiseThr);
-     MI=eigVectSignal*inv(eigVectSignal'*eigVectSignal)*eigVectSignal';
-     % noise subspace
-     MI=eye(size(MI))-MI;
+%      % eigen decomposition technique #1   
+%      [eigVector, eigValue] = eig(Rxx);
+%      eigValue = ((diag(eigValue)));
+%      % signal and noise eigenvalue threshold
+%      eigNoiseThr = 1e-6;
+%      % signal subspace
+%      eigVectSignal = eigVector(:, eigValue > eigNoiseThr);
+%      MI=eigVectSignal*inv(eigVectSignal'*eigVectSignal)*eigVectSignal';
+%      % noise subspace
+%      MI=eye(size(MI))-MI;
 
-    % eigen decomposition technique #1
+
+[E, D] = eig(Rxx); %eigenvalues and eigenvectors
+D = diag(D);
+[~,I] = sort(D,'descend');
+
+E = E(:,I); 
+En = E(:,M+1:end);% eigenvectors of noise subspace
+
+Pn = En * inv(En'*En) * En';
+   
+% eigen decomposition technique #1
     % can't get accurate results in 2D 
-%   Auto-correlation matrix
+%  Auto-correlation matrix
 %     Rxx = Rsig*Rsig'/312;
-%   eigenvalue decomposition
+% %  eigenvalue decomposition
 %     [Vi,Li] = eig(Rxx);
-%   sort in descending order
+% %  sort in descending order
 %     [L,I] = sort(diag(Li),'descend');
 %     V = Vi(:,I);
 % %   Signal Subspace
 %     Ps = V(:,1:M)*V(:,1:M)';
 % %   Noise Subspace
 %     Pn = V(:,1+M:N)*V(:,1+M:N)';
- 
+
     theta1=[0:1:90];
-    tau=[0:1e-7:5e-6];
+    tau=[0:1e-9:5e-8];
     deltaF = 30e3;
     % The MUSIC spectrum
 
@@ -42,10 +53,32 @@ function [degrees,delay] = MUSIC(Rsig,M,N,d,lam,subcarr)
         end
         for j=1:length(tau)
            for l = 1:subcarr/2
-                A(:,l) = exp(2*pi*-1i*l*deltaF*tau(j));
+                A(:,l) = exp(2*pi*-1i*(l*deltaF)*tau(j));
            end
-           A1 = kron(A.',B);
-           PMUSIC(i,j)= (subcarr/2)/(sum(abs(A1'*MI*A1)));
+           A1 = kron(B,A.');
+           PMUSIC(i,j)= 1/(sum(abs(A1'*Pn*A1)));
        end
     end
+ 
+    [C,I] = max(PMUSIC(:));
+    [I1,I2] = ind2sub(size(PMUSIC),I);
+    deg = I1-1;
+    del = I2-1;
+%     figure(1);
+%     plot(theta1,10*log10(PMUSIC));
+%     title('MUSIC spectrum');
+%     xlabel('Angle [degrees]');
+%     ylabel('PMUSIC');   
+    figure(1);
+    [X,Y] = meshgrid(theta1,tau);
+    surf(X,Y,10*log10(PMUSIC)')
+    shading interp 
+    colorbar    
+    title('MUSIC spectrum');
+    xlabel('Angle [degrees]');
+    ylabel('Delay (s)');
+    zlabel('PMUSIC');
     
+    degrees = deg;
+    delay = del;
+end
